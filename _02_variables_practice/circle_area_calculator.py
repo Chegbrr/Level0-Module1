6744db5e8ce3f317b4804c29d7f5d20077151449
Chegbrr <cahitegebalci@gmail.com>
2024-08-07 11:56:29 -0700

    j

--- _02_variables_practice/circle_area_calculator.py
+++ _02_variables_practice/circle_area_calculator.py
@@ -2,7 +2,7 @@ import turtle
 from tkinter import messagebox, simpledialog, Tk
 import math
 
-# Goal: Write a Python program that asks the user for the radius 
+# Goal: Write a Python program that asks the user for the radius
 #       of a circle and displays the area of that circle.
 #       The formula for the area of a circle is πr^2.
 #       See example image in package to check your output.
@@ -10,25 +10,27 @@ import math
 if __name__ == '__main__':
     window = Tk()
     window.withdraw()
-    
+
     # Ask the user for the radius in pixels and store it in a variable
     # simpledialog.askinteger()
-    
+    rad = simpledialog.askinteger(title='', prompt='Enter the radius of a circle to know the area. ')
+
     # Make a new turtle
-    
+    turt = turtle.Turtle()
     # Have your turtle draw a circle with the correct radius
     # my_turtle.circle()
-
+    turt.circle(radius=rad)
     # Call the turtle .penup() method
-
+    turt.penup()
     # Move your turtle to a new x,y position using .goto()
-
+    turt.goto(-250,-100)
     # Calculate the area of your circle and store it in a variable
     # Hint, you can use math.pi
-    
+    area = (rad^2)*math.pi
     # Write the area of your circle using the turtle .write() method
     # my_turtle.write(arg="area = " + str(area), move=True, align='left', font=('Arial',8,'normal'))
-
+    turt.write(arg="area = " + str(area), move=True, align = 'left', font=('Arial', 50, 'normal'))
     # Hide your turtle
-
+    turt.hideturtle()
     # Call turtle.done()
+turtle.done()
--- _03_if_else/_3_tasty_tomato/tasty_tomato.py
+++ _03_if_else/_3_tasty_tomato/tasty_tomato.py
@@ -1,5 +1,6 @@
 from tkinter import *
 import tkinter as tk
+from tkinter import simpledialog
 
 window_width = 600
 window_height = 600
@@ -11,13 +12,36 @@ canvas.grid()
 
 # 1. Ask the user what color tomato they would like and save their response
 #    You can give them up to three choices
-
+q1 = simpledialog.askstring(title= '', prompt= 'What color would you like your tomato base. (Red, purple, black, or yellow) ')
+q2 = simpledialog.askstring(title= '', prompt= 'What color would you like the stem of the tomato. (Red, green, yellow, or purple)')
 
 # 2. Use if-else statements to draw the tomato in the color that they chose
 #    You can modify the code below or draw your own tomato
-canvas.create_oval(75, 200, 400, 450, fill="red", outline="")
-canvas.create_oval(200, 200, 525, 450, fill="red", outline="")
+if q1 == 'red':
+    canvas.create_oval(75, 200, 400, 450, fill="red", outline="")
+    canvas.create_oval(200, 200, 525, 450, fill="red", outline="")
+else:
+    canvas.create_oval(75, 200, 400, 450, fill="red", outline="")
+    canvas.create_oval(200, 200, 525, 450, fill="red", outline="")
+if q1 == 'purple':
+    canvas.create_oval(75, 200, 400, 450, fill="purple", outline="")
+    canvas.create_oval(200, 200, 525, 450, fill="purple", outline="")
+if q1 == 'black':
+    canvas.create_oval(75, 200, 400, 450, fill="black", outline="")
+    canvas.create_oval(200, 200, 525, 450, fill="black", outline="")
+if q1 == 'yellow':
+    canvas.create_oval(75, 200, 400, 450, fill="yellow", outline="")
+    canvas.create_oval(200, 200, 525, 450, fill="yellow", outline="")
 
-canvas.create_rectangle(275, 100, 325, 230, fill="green", outline="")
 
+if q2 == 'red':
+    canvas.create_rectangle(275, 100, 325, 230, fill="red", outline="")
+if q2 == 'green':
+    canvas.create_rectangle(275, 100, 325, 230, fill="green", outline="")
+if q2 == 'yellow':
+    canvas.create_rectangle(275, 100, 325, 230, fill="yellow", outline="")
+if q2 == 'purple':
+    canvas.create_rectangle(275, 100, 325, 230, fill="purple", outline="")
+else:
+    canvas.create_rectangle(275, 100, 325, 230, fill="green", outline="")
 root.mainloop()
